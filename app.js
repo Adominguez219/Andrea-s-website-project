e2234ba2a813aaf608ec6b1ea65dc956a2bdf813
Adominguez219 <adominguez219@student.alamo.edu>
2019-12-12 13:31:30 -0600

    src

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const navSlide =() => {
+const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.list');
 	const navLinks = document.querySelectorAll('.list li');
 //toggle Nav
-	burger.addEventListner('click', () => {
+	burger.addEventListner('click',()=>{
 		nav.classList.toggle('nav-active');
 
 	});
