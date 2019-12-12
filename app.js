eb0007eedcedbdaef18ba9ad885bdbc07521b568
Adominguez219 <adominguez219@student.alamo.edu>
2019-12-12 14:07:03 -0600

    js

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.list');
 	const navLinks = document.querySelectorAll('.list li');
-//toggle Nav
-	burger.addEventListner('click',()=>{
+	//toggle Nav
+	burger.addEventListner('click', () => {
 		nav.classList.toggle('nav-active');
 
 	});
-//Animate Links 
-	navLinks.foreach((link, index) => {
-		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-	});
+
 }
 
 
-navSlide();
+const app = ()=>{
+	navSlide();
+}
